befee285ff9f189377d56235cb837e02c9732eae
Yarib Díaz <yaribdiazz@gmail.com>
2023-08-07 12:02:49 -0600

    first build

--- src/components/MailBar.jsx
+++ src/components/MailBar.jsx
@@ -9,7 +9,7 @@ const MailBar = () => {
     <div className="w-5/6 sm:px-0 text-white">
 
         <div className='flex justify-center w-10'>
-            <button onClick={() => navigate-1} className='p-2  text-black rounded-2xl hover:bg-gray-300/[0.70] hover:px-4 transition-all duration-500'>
+            <button onClick={() => navigate(-1)} className='p-2  text-black rounded-2xl hover:bg-gray-300/[0.70] hover:px-4 transition-all duration-500'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
             </svg>
